f7474b483d61083ed5736ffaa449812258613041
Eden-ETEOT <eden.condominios@gmail.com>
2026-06-09 17:31:29 -0300

    EDIT branches

--- 2026/landing-page/js/script.js
+++
@@ -1,9 +0,0 @@
-const perguntasFaq = document.querySelectorAll('.pergunta-faq');
-
-perguntasFaq.forEach((pergunta) => {
-    pergunta.addEventListener('click', () => {
-        const itemFaq = pergunta.closest('.item-faq');
-
-        itemFaq.classList.toggle('ativo');
-    });
-});

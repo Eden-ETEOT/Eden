9d830f7686810f565be612a2e13b354a6d2cf04d
Guilherme Ferreira <guilherme.edenteam@gmail.com>
2026-06-23 14:59:28 -0300

    add configs de conexão
    cria conexão php e adiciona o banco de dados no repositório
    
    Usei PDO

---
+++ 2026/config/conexao.php
@@ -0,0 +1,16 @@
+<?php 
+    $host = "localhost";
+    $banco   = "eden";
+    $usuario = "root";
+    $senha = "";
+
+try {
+    $conexao = new PDO("mysql:host=$host;dbname=$banco;charset=utf8", $usuario, $senha);
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
+catch (Exception $e) {
+    die("Erro genérico: " . $e->getMessage());
+}
+?>

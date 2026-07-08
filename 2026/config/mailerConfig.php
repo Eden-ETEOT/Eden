1a628f1b69e1786f5b824c8cfc6178e9dda0ed1e
guilhermeedenteam <guilherme.edenteam@gmail.com>
2026-07-08 08:58:18 -0300

    edit mailerConfig

--- 2026/config/mailerConfig.php
+++ 2026/config/mailerConfig.php
@@ -6,6 +6,6 @@ define('MAIL_ENCRYPTION', 'tls'); // Gmail usa STARTTLS na porta 587
 define('MAIL_USERNAME', 'guilherme.edenteam@gmail.com');   // <-- preencha
 define('MAIL_PASSWORD', 'qixj xvwg ttdk fwgf'); // <-- preencha (App Password de 16 dígitos)
 define('MAIL_FROM_ADDRESS', 'guilherme.edenteam@gmail.com'); // <-- geralmente igual ao MAIL_USERNAME
-define('MAIL_FROM_NAME', 'Éden Systems');
+define('MAIL_FROM_NAME', 'Eden Systems');
 
 ?>

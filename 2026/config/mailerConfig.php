be9cca5f384580799db9fa20523beaeab67f33d2
guiifl <guilherme.edenteam@gmail.com>
2026-08-27 17:38:13 -0300

    edit mailerconfig
    muda senha de app

--- 2026/config/mailerConfig.php
+++ 2026/config/mailerConfig.php
@@ -4,7 +4,7 @@ define('MAIL_HOST', 'smtp.gmail.com');
 define('MAIL_PORT', 587);
 define('MAIL_ENCRYPTION', 'tls'); // Gmail usa STARTTLS na porta 587
 define('MAIL_USERNAME', 'carinha785@gmail.com');   // <-- preencha
-define('MAIL_PASSWORD', 'devy geza axyx apdz'); // <-- preencha (App Password de 16 dígitos)
+define('MAIL_PASSWORD', 'ywev cwcn wbkf liix'); // <-- preencha (App Password de 16 dígitos)
 define('MAIL_FROM_ADDRESS', 'carinha785@gmail.com'); // <-- geralmente igual ao MAIL_USERNAME
 define('MAIL_FROM_NAME', 'Eden Systems');
 

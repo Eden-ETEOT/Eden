f7dab87e21246832b3cc6b5652c6eeda17a24133
guiifl <guilherme.edenteam@gmail.com>
2026-08-27 16:29:24 -0300

    edit mailer config
    altera o email

--- 2026/config/mailerConfig.php
+++ 2026/config/mailerConfig.php
@@ -3,9 +3,9 @@
 define('MAIL_HOST', 'smtp.gmail.com');
 define('MAIL_PORT', 587);
 define('MAIL_ENCRYPTION', 'tls'); // Gmail usa STARTTLS na porta 587
-define('MAIL_USERNAME', 'guilherme.edenteam@gmail.com');   // <-- preencha
-define('MAIL_PASSWORD', 'qixj xvwg ttdk fwgf'); // <-- preencha (App Password de 16 dígitos)
-define('MAIL_FROM_ADDRESS', 'guilherme.edenteam@gmail.com'); // <-- geralmente igual ao MAIL_USERNAME
+define('MAIL_USERNAME', 'carinha785@gmail.com');   // <-- preencha
+define('MAIL_PASSWORD', 'devy geza axyx apdz'); // <-- preencha (App Password de 16 dígitos)
+define('MAIL_FROM_ADDRESS', 'carinha785@gmail.com'); // <-- geralmente igual ao MAIL_USERNAME
 define('MAIL_FROM_NAME', 'Eden Systems');
 
 ?>

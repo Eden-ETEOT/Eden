0dec6d25e79324ebea879ff37a4480fdd7bc99d8
guiifl <guilherme.edenteam@gmail.com>
2026-08-28 11:16:11 +0000

    edit config conexao + htaccess + gitignore

--- 2026/config/conexao.php
+++ 2026/config/conexao.php
@@ -1,16 +1,16 @@
-<?php 
-    $host = "localhost";
-    $banco   = "eden";
-    $usuario = "root";
-    $senha = "";
+<?php
+$config = require __DIR__ . '/database.php';
+
+$host     = $config['host'];
+$banco    = $config['database'];
+$usuario  = $config['username'];
+$senha    = $config['password'];
 
 try {
     $conexao = new PDO("mysql:host=$host;dbname=$banco;charset=utf8", $usuario, $senha);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
-}
-catch (Exception $e) {
+} catch (Exception $e) {
     die("Erro genérico: " . $e->getMessage());
 }
-?>
